48a175a1f0e851b3750826ae99206aa093b34770
trinhmatthew@gmail.com <trinhmatthew@gmail.com>
2017-10-24 19:40:09 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+#DrumKit
+
+Simple drum web app made to practice using different data types
+
+Built with vanilla JS
+
+To do:
+
+-Change the volume of individual sounds (kick, snare, etc..)
+
+-Add the ability to create drum loops
+
+-Add the ability to save and export drum loops
---
+++ app.js
@@ -0,0 +1,53 @@
+function playSound(keyCode) {
+  var audio = document.getElementById(keyCode)
+  audio.currentTime = 0;
+  audio.play();
+  audio.parentElement.classList.add('played')
+}
+
+var keySounds = window.addEventListener('keypress', function(e) {
+  if (e.keyCode === 97) {
+    playSound(97);
+  };
+  if (e.keyCode === 115) {
+    playSound(115)
+  };
+  if (e.keyCode === 100) {
+    playSound(100)
+  };
+  if (e.keyCode === 102) {
+    playSound(102)
+  };
+  if (e.keyCode === 103) {
+    playSound(103)
+  };
+  if (e.keyCode === 104) {
+    playSound(104)
+  };
+  if (e.keyCode === 106) {
+    playSound(106)
+  };
+  if (e.keyCode === 107) {
+    playSound(107)
+  };
+  if (e.keyCode === 108) {
+    playSound(108)
+  };
+});
+
+function removeTransition(e){
+  if (e.propertyName === 'transform') {
+    e.path[0].classList.remove('played')
+  }
+}
+
+var allPads = document.getElementsByClassName('pad');
+
+//Play sound by mouseclick
+for (i=0; i<allPads.length; i++) {
+  allPads[i].addEventListener('transitionend', removeTransition);
+  allPads[i].addEventListener('click', function(event){
+    this.getElementsByTagName('audio')[0].currentTime = 0;
+    this.getElementsByTagName('audio')[0].play();
+  })
+}
